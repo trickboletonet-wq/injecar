04795623f20a366a5eb00997ffffba41669b2413
trickmtgaonline-blip <trickmtgaonline@gmail.com>
2026-08-27 19:53:35 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const mapsUrl = 'https://www.google.com/maps/search/?api=1&query=Rua+das+Adália
 const routeUrl = 'https://www.google.com/maps/dir/?api=1&destination=Rua+das+Adálias+37+Cecília+Viamão+RS';
 
 const images = {
-  logo: '/injecar-logo-transparent.png',
+  logo: '/126a14e9-a05c-468f-8250-a6a97c989fc9.png',
   hero: '/730210568_18331418242261610_4238321417700025183_n.jpg',
   workshop: '/681048939_18317284393261610_8019736608153311643_n.jpg',
   mechanic: '/728951157_18325759207261610_3028685046551557509_n.jpg',
